fa24a2aff2fc9d94178d68ef437a2c5ad3b16d58
Matthew Bunday <mkbunday@gmail.com>
2021-11-09 14:18:14 -0500

    Finish up with Mona Lisa computation

--- pages/api/discord.ts
+++ pages/api/discord.ts
@@ -14,17 +14,22 @@ export const provider = new ethers.providers.InfuraProvider(
   process.env.INFURA_PROJECT
 );
 
-const SQUID_WETH_PAIR = "0xfad704847967d9067df7a60910399155fca43fe8";
+const DOG_WETH_PAIR = "0xc96f20099d96b37d7ede66ff9e4de59b9b1065b1";
 const USDC_WETH_PAIR = "0x397ff1542f962076d0bfe58ea045ffa2d347aca0";
 
-const squidWethSushiswap = new Contract(SQUID_WETH_PAIR, abi, provider);
+const DOG_SUPPLY = 16_969_696_969;
+const MONA_LISA_VALUE = 870_000_000;
+
+const dogWethSushiswap = new Contract(DOG_WETH_PAIR, abi, provider);
 
 const usdcWethSushiswap = new Contract(USDC_WETH_PAIR, abi, provider);
 
-async function getSquidEthPrice() {
-  const [r0, r1] = await squidWethSushiswap.functions["getReserves"]();
+async function getDogEthPrice() {
+  const [r0, r1] = await dogWethSushiswap.functions["getReserves"]();
 
-  const price = r1.div(r0).div(1e9);
+  console.log(r0.toString(), r1.toString());
+
+  const price = r1 / r0;
   return price;
 }
 
@@ -33,6 +38,19 @@ async function getEthUsdPrice() {
 
   const price = r0.div(r1.div(1e12));
   return price;
+}
+
+async function getDogUsdPrice() {
+  const [dogEthPrice, ethUsdPrice] = await Promise.all([
+    getDogEthPrice(),
+    getEthUsdPrice(),
+  ]);
+
+  return dogEthPrice * ethUsdPrice;
+}
+
+async function monaLisaPercentage(dogUsdPrice: number) {
+  return ((dogUsdPrice * DOG_SUPPLY) / MONA_LISA_VALUE) * 100;
 }
 
 type Data = {
@@ -44,35 +62,33 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   const client = new Client({ ws: { intents: [Intents.FLAGS.GUILDS] } });
-  const [price, ethPrice, _] = await Promise.all([
-    getSquidEthPrice(),
-    getEthUsdPrice(),
+  const [dogUsdPrice, _] = await Promise.all([
+    getDogUsdPrice(),
     client.login(process.env.DISCORD_TOKEN),
   ]);
+  const monaLisaP = monaLisaPercentage(dogUsdPrice);
 
   const guilds = client.guilds.cache;
   await Promise.all(
     guilds.map(async (guild) => {
       const g = await guild.fetch();
       const n = g.me?.nickname;
-      const lastPrice = parseInt(n?.match(/^Ξ(\d+)/)?.[1] || "");
-      const isFlat = lastPrice == price;
-      const updown = isFlat ? "→" : lastPrice < price ? "↗" : "↘";
+      // const lastPrice = parseInt(n?.match(/^Ξ(\d+)/)?.[1] || "");
+      // const isFlat = lastPrice == price;
+      // const updown = isFlat ? "→" : lastPrice < price ? "↗" : "↘";
 
-      console.log({
-        lastPrice,
-        price: price.toString(),
-        usd: price * ethPrice,
-      });
+      // console.log({
+      //   lastPrice,
+      //   price: price.toString(),
+      //   usd: price * ethPrice,
+      // });
 
-      g.me?.setNickname(
-        `Ξ${price} ${updown}${isFlat ? "" : Math.abs(lastPrice - price)}`
-      );
+      g.me?.setNickname(`${(await monaLisaP).toPrecision(3)}%`);
     })
   );
 
   await client.user?.setPresence({
-    activity: { name: `\$${price * ethPrice}`, type: 3 },
+    activity: { name: `\$${dogUsdPrice.toPrecision(4)}`, type: 3 },
     status: "online",
   });
 
